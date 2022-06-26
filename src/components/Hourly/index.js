9f3c6ada302e395ed578ed96ed8810b56e98b979
adriankorp <adrian.korpowski@gmail.com>
2022-06-26 15:54:28 +0200

    Add Hourly

---
+++ src/components/Hourly/index.js
@@ -0,0 +1,27 @@
+import "./index.css";
+
+const Hourly = (props) => (
+  <div className="conteiner-hourly">
+    {props.hourly.map((el, idnex) => {
+      console.log(el);
+      return (
+        <div className="hourly">
+          <p>
+            {el.dt_txt.split(" ")[1]} <br />
+            {el.dt_txt.split(" ")[0]}
+          </p>
+          <div className="image">
+            <img
+              src={`http://openweathermap.org/img/wn/${el.weather[0].icon}.png`}
+              alt="weather icon"
+            />
+          </div>
+          <p>{parseInt(el.main.temp) + "°C"}</p>
+          <p>{el.weather[0].description}</p>
+        </div>
+      );
+    })}
+  </div>
+);
+
+export default Hourly;
