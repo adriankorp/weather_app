e4c5ca7d9aed2fa8e01ac5123f693f2bddb34aa0
adriankorp <adrian.korpowski@gmail.com>
2022-06-26 18:47:21 +0200

    Add header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
-import Home from './components/Home';
+import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <Home></Home>
+    <>
+      <header>
+        <h1>Weather app</h1>
+      </header>
+      <Home />
+    </>
   );
 }
 
